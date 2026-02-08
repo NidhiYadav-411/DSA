9a4c25e99b94759a275e5693f09bca67b406c94e
NidhiYadav-411 <nidhiyadav21346@gmail.com>
2026-02-09 00:27:32 +0530

    Time: 1 ms (99.75%) | Memory: 47.4 MB (57.32%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int size = n * n;
+
+        boolean[] seen = new boolean[size + 1];
+        int a = -1; // repeated
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = grid[i][j];
+                sum += val;
+
+                if (seen[val]) {
+                    a = val;
+                }
+                seen[val] = true;
+            }
+        }
+
+        int totsum = (size * (size + 1)) / 2;
+        int b = totsum - (sum - a); // missing
+
+        return new int[]{a, b};
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{1, 2}, {2, 4}};
+        int[] ans = findMissingAndRepeatedValues(grid);
+        System.out.println(Arrays.toString(ans));
+    }
+}
