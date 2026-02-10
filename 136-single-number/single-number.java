2f061fd606ff35f195e36ac80d29bc342a146a5e
NidhiYadav-411 <nidhiyadav21346@gmail.com>
2026-02-10 22:35:27 +0530

    Time: 526 ms (6.73%) | Memory: 47.2 MB (13.30%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,59 @@
+class Solution {
+    public static int singleNumber(int[] nums) {
+        int n=nums.length ;
+
+        for(int i=0; i<=n-1;i++){
+            for (int j=0; j<n-i-1;j++){
+                if (nums[j]> nums[j+1]){
+                  int temp=nums[j];
+                  nums[j]=nums[j+1];
+                   nums[j+1]=temp;
+
+                 
+                }
+
+            }
+        }
+        if (nums.length == 1) {
+           return nums[0];
+}
+
+        int ans =-1;
+
+        for (int i=0;i<n;i++){
+            if (i==0){
+                if (nums[i]!=nums[i+1]){
+                    ans =nums[i];
+                    break;
+
+
+                }
+            }else if (i==n-1){
+                if (nums[i]!=nums[i-1]){
+                    ans=nums[i];
+                    break;
+                }
+            }
+            else {
+                if (nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+                    ans=nums[i];
+                    break;
+                }
+
+            }
+        }
+
+        return ans  ;
+
+        
+    }
+    public static void main  (String [] args ){
+        int nums[]={1,1,2,3,3};
+         
+         int result= singleNumber(nums);
+         System.out.println (result);
+
+
+
+    }
+}
