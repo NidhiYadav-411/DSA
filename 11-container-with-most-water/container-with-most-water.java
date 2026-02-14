42df5713946fd122267b86cbc8b6ceb53223a047
NidhiYadav-411 <nidhiyadav21346@gmail.com>
2026-02-15 00:19:15 +0530

    Time: 5 ms (80.51%) | Memory: 77.6 MB (10.68%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,38 +1,25 @@
-import java.util.*;  // Import utilities
-
-
 class Solution {
-   // Function to calculate the maximum area of water container
-   public int maxArea(int[] height) {
-       int n = height.length;
-       int maxWater = 0;
-       int left = 0, right = n - 1; // Initialize two pointers
+    public int maxArea(int[] height) {
+        int n= height.length;
+        int i=0;
+        int j=n-1;
+        int maxwater=0;
 
 
-       // Continue until pointers meet
-       while (left < right) {
-           int width = right - left; // Width between lines
-           int minHeight = Math.min(height[left], height[right]); // Height is min of two lines
-           maxWater = Math.max(maxWater, width * minHeight); // Update max area
+        while(i<j){
+            int weight =j-i;
+            int minheight =Math.min(height[i],height[j]);
+            int currweight=weight*minheight;
+            maxwater=Math.max(maxwater,currweight);
+            if (height[i]<height[j]){
+                i++;
+            }else{
+                j--;
+            }
 
+        }
 
-           // Move the pointer pointing to the smaller height
-           if (height[left] < height[right])
-               left++;
-           else
-               right--;
-       }
-
-
-       return maxWater;
-   }
-
-
-   public static void main(String[] args) {
-       Solution sol = new Solution();
-       int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7}; // Input array
-       int result = sol.maxArea(height); // Call function
-       System.out.println("Maximum water that can be stored: " + result);
-   }
+        
+        return maxwater;
+    }
 }
-
